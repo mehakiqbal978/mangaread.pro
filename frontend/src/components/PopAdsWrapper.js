57232a8f9b49ec364281bea0bacbb1b7f778b0fb
mehak iqbal <mehakiqbal974@gmail.com>
2026-09-23 05:28:52 +0500

    Update pop ad script

--- frontend/src/components/PopAdsWrapper.js
+++ frontend/src/components/PopAdsWrapper.js
@@ -11,14 +11,8 @@ export default function PopAdsWrapper() {
 
   return (
     <script
-      type="text/javascript"
-      data-cfasync="false"
       dangerouslySetInnerHTML={{
-        __html: `
-/*<![CDATA[/* */
-(function(){var j=window,u="f80ae73ce1da56229e8abb10a7133550",c=[["siteId",373*362-441+5186054],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],h=["d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL2xpL0kveWdtYWlsLm1pbi5qcw==","ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvc2luZGV4Lm1pbi5qcw=="],l=-1,t,p,o=function(){clearTimeout(p);l++;if(h[l]&&!(1815825597000<(new Date).getTime()&&1<l)){t=j.document.createElement("script");t.type="text/javascript";t.async=!0;var r=j.document.getElementsByTagName("script")[0];t.src="https://"+atob(h[l]);t.crossOrigin="anonymous";t.onerror=o;t.onload=function(){clearTimeout(p);j[u.slice(0,16)+u.slice(0,16)]||o()};p=setTimeout(o,5E3);r.parentNode.insertBefore(t,r)}};if(!j[u]){try{Object.freeze(j[u]=c)}catch(e){}o()}})();
-/*]]>/* */
-        `
+        __html: `(function(s){s.dataset.zone='11865842',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
       }}
     />
   );
